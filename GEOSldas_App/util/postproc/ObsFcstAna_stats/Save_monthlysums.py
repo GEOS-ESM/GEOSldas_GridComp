6165af823cff975d325e0c61e82894936167bd2b
Rolf Reichle <rolf.reichle@nasa.gov>
2025-05-20 16:55:21 -0400

    cleaner separation of user-defined inputs and processing code (Save_monthlysums.py)

--- GEOSldas_App/util/postproc/ObsFcstAna_stats/Save_monthlysums.py
+++ GEOSldas_App/util/postproc/ObsFcstAna_stats/Save_monthlysums.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 
 """
-Sample script for processing data assimilation (DA) diagnostics from 
-GEOSldas ObsFcstAna output into monthly sums. Long-term statistics can be computed
-efficiently later using the monthly data. 
-
-Processing workflow:
-1. User provides experiment information and calls postproc_ObsFcstAna to:
-   - Calculate/save monthly sums of Obs/Fcst/Ana and squared/products
-   - Calculate mean/stdv of Obs/Fcst/Ana
-2. User computes desired DA diagnostic statistics and creates plots
+Sample script for computing and storing monthly sums and sums of squares and cross-products
+from GEOSldas ObsFcstAna output.  Data assimilation diagnostics such as the mean and
+std-dev of the observation-minus-forecast residuals can be diagnosed quickly from these
+intermediate "sums" files.
 
 Usage on Discover:
     $ module load python/GEOSpyD
-    $ ./Main_example.py
+    $ ./Save_monthlysums.py
     
     # Background execution:
-    $ nohup ./Main_example.py > out.log &
+    $ nohup ./Save_monthlysums.py > out.log &
 
-Author: Q. Liu, R. Reichle, A. Fox
-Last Modified: May., 2025
+Authors: Q. Liu, R. Reichle, A. Fox
+Last Modified: May 2025
 """
 
 import sys;       sys.path.append('../../shared/python/')
 import warnings;  warnings.filterwarnings("ignore")
 import os
 
-import numpy             as np
+import numpy as np
+
 from datetime               import datetime, timedelta
-
 from read_GEOSldas          import read_tilecoord, read_obs_param
-
 from postproc_ObsFcstAna    import postproc_ObsFcstAna
 
-# Uncomment if to run the script in the background to see the standard output while running 
-# import io
+# If the script is run in the background, uncomment the following lines to see the redirected
+#   standard output in the out.log file immediately.  When the lines are commented out, the redirected
+#   standard output will not appear in the out.log file until the job has completed.
+# If the script is run in the foreground, the lines must be commented out.
+#
+#import io
 #sys.stdout = io.TextIOWrapper(open(sys.stdout.fileno(), 'wb', 0), write_through=True)
 #sys.stderr = io.TextIOWrapper(open(sys.stderr.fileno(), 'wb', 0), write_through=True)
 
-# User provided time range for processing (Year, Month, Day)
-start_time = datetime(2016,4,1)
-end_time   = datetime(2016,5,1)
+# --------------------------------------------------------------------------------------------------
+#
+# User-defined inputs
 
-# -------------------------------- Experiments Information -----------------------------------------
+# Range of months to process:
+
+start_year  = 2016
+start_month =    4
+last_year   = 2016
+last_month  =    4
+
+# Experiments to process:
+
 # Supports single experiment or multiple experiments.
 # All experiments must have identical tilecoords and number/order of observation species.
 # If the default "species" number/order do not match, need to set the *optional*
@@ -50,45 +56,78 @@ end_time   = datetime(2016,5,1)
 # This capability is required to enable calculating OmF/OmA statistics for one experiment
 #   using observations from another experiment. See note below.
 
-# User provide at lease one experiment 
-exp_main = { 'expdir'       : '/discover/nobackup/projects/gmao/merra/iau/merra_land/SMAP_runs/SMAP_Nature_v11/',
-          'expid'        : 'DAv8_SMOSSMAP',
-          'exptag'       : 'DAMulti_SMAP', 
-          'domain'       : 'SMAP_EASEv2_M36_GLOBAL',
-          'da_t0'        : 3,       # first hour of the month 
-          'da_dt'        : 10800,   # ObsFcstAna file interval in seconds
-          'species_list' : [5,6,7,8] }
+# User must provide at least one experiment 
+
+exp_main = {
+    'expdir'        : '/discover/nobackup/projects/gmao/merra/iau/merra_land/SMAP_runs/SMAP_Nature_v11/',
+    'expid'         : 'DAv8_SMOSSMAP',
+    'exptag'        : 'DAMulti_SMAP', 
+    'domain'        : 'SMAP_EASEv2_M36_GLOBAL',
+    'da_t0'         : 3,                              # hour of first land analysis
+    'da_dt'         : 10800,                          # ObsFcstAna file interval in seconds
+    'species_list'  : [5,6,7,8],
+    'obsparam_time' : "20150401_0000"                 # YYYYMMDD_HHMM
+}
 
 # Optional experiment can be added for cross masking or extracting obs from a different experiment
-exp_sup1 = { 'expdir'       : '/discover/nobackup/projects/gmao/merra/iau/merra_land/SMAP_runs/SMAP_Nature_v11/',
-          'expid'        : 'DAv8_M36',
-          'exptag'       : 'DASMAP_SMAP', 
-          'domain'       : 'SMAP_EASEv2_M36_GLOBAL',
-          'species_list' : [1,2,3,4]  }
 
-# User provided directory to store monthly sum files
-# can use the main experiment's ana/ or a different path
-monthly_sums_path = '/discover/nobackup/qliu/SMAP_diag/' +exp_main['expid']+ \
-              '/output/'+exp_main['domain']+'/ana/ens_avg/'
+exp_sup1 = {
+    'expdir'        : '/discover/nobackup/projects/gmao/merra/iau/merra_land/SMAP_runs/SMAP_Nature_v11/',
+    'expid'         : 'DAv8_M36',
+    'exptag'        : 'DASMAP_SMAP', 
+    'domain'        : 'SMAP_EASEv2_M36_GLOBAL',
+    'species_list'  : [1,2,3,4],
+    'obsparam_time' : "20150401_0000"                 # YYYYMMDD_HHMM    
+}
+
+# User provided directory to store monthly sum files;  can use the main experiment's ana/ or a different path
+
+out_path = '/discover/nobackup/qliu/SMAP_diag/'
 
 # forecasts and analyses are always from the main experiment.
 # observations can be from experiment indicated by 'obs_from' index.
 # The mostly likely use case for this is that _scaled_ observations from a DA experiment
 # are used to compute OmF etc diagnostics for a corresponding open loop experiment.
 
+obs_from = 1              # take obs from "exp_sup1" (0-based indexing)
+
+# ---------------------------------------------------------------------------------------------------------
+
+# process time range info;  end_time is first of month after (end_year, end_month)
+
+if last_month==12 :
+    end_month = 1
+    end_year  = last_year + 1
+else :
+    end_month = last_month + 1
+    end_year  = last_year
+    
+start_time = datetime( start_year, start_month, 1)
+end_time   = datetime( end_year,   end_month,   1)
+
+# assemble output path
+
+monthly_sums_path = out_path + '/' + exp_main['expid']+ '/output/' + exp_main['domain'] + '/ana/ens_avg/'
+
+# process experiment info
+
 exp_list = [exp_main, exp_sup1]
-obs_from = 1                            # obs is from "exp_sup1" (0-based indexing)
+
 if obs_from >= len(exp_list):
     print('Invalid "obs_from" value')
     sys.exit()
 
 # Read tilecoord and obs_param information of each experiment
+
 for exp in exp_list:
     expdir   = exp['expdir']
     expid    = exp['expid']
     domain   = exp['domain']
+
+    YYYY     = exp['obsparam_time'][0:4]
+    MM       = exp['obsparam_time'][4:6]
     
-    fop      = expdir+expid+'/output/'+domain+'/rc_out/Y2015/M04/'+expid+'.ldas_obsparam.20150401_0000z.txt'
+    fop      = expdir+expid+'/output/'+domain+'/rc_out/Y'+YYYY+'/M'+MM+'/'+expid+'.ldas_obsparam.'+exp[obsparam_time]+'z.txt'
     obsparam = read_obs_param(fop)
 
     # get the species list and default to list of all species if doesn't exist 
@@ -106,14 +145,14 @@ for exp in exp_list:
 
     exp.update({'tilecoord':tc,'obsparam':obsparam})
 
-#  =========================================================================
-#  Postprocess raw ObsFcstAna output data into monthly sums for simpler and faster postprocessing;
-#  computes mean, variance from monthly sums that can be used to compute DA diagnostics directly
+# Postprocess raw ObsFcstAna output data into monthly sums 
 
 # Initialize the postprocessing object
 postproc = postproc_ObsFcstAna(exp_list, start_time, end_time, obs_from=obs_from)
-# Compute and save monthly sums 
+
+# Compute and save monthly sums
 postproc.save_monthly_sum(monthly_sums_path)
 
-print('Processing completed!')   
+print('Save_monthlysums: Processing completed!')   
+
 # ====================== EOF =========================================================
