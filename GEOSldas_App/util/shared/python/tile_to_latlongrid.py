88739fa6ddab3977c2bbbe0587a3222487b71cef
Qing Liu <qing.liu@nasa.gov>
2025-06-24 16:59:25 -0400

    change max to mean tile values for grid

--- GEOSldas_App/util/shared/python/tile_to_latlongrid.py
+++ GEOSldas_App/util/shared/python/tile_to_latlongrid.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # 
         
-def tile_to_latlongrid( tile_data, tile_coord, resolution, nodata=1.e15, nodata_tol=1.e-4):
+def tile_to_latlongrid( tile_data, tile_coord, resolution, nodata=1.e15, nodata_tol_frac=1.e-4):
     
     """
     Map (1d) tile-space data onto (2d) regular lat/lon grid of arbitrary "resolution".
@@ -26,7 +26,7 @@ def tile_to_latlongrid( tile_data, tile_coord, resolution, nodata=1.e15, nodata_
     
     """
     
-    tile_data[np.abs(tile_data - nodata) < nodata_tol]  = np.nan
+    tile_data[np.abs(tile_data - nodata) < nodata*nodata_tol_frac]  = np.nan
     
     # Verify input datasize
     if tile_data.shape[0] != tile_coord['N_tile']:
@@ -39,25 +39,26 @@ def tile_to_latlongrid( tile_data, tile_coord, resolution, nodata=1.e15, nodata_
 
     nf = tile_data.shape[-1]
 
-    lat_grid = np.arange( -90+resolution/2,  90, resolution)
-    lon_grid = np.arange(-180+resolution/2, 180, resolution)
+    lat_grid = np.arange( -90+resolution/2,  90+resolution/2, resolution)
+    lon_grid = np.arange(-180+resolution/2, 180+resolution/2, resolution)
 
     lon_2d, lat_2d = np.meshgrid(lon_grid, lat_grid)
 
-    grid_data = np.full([len(lat_grid), len(lon_grid), nf], np.nan)
+    grid_data = np.zeros((len(lat_grid), len(lon_grid), nf))
+    N_tilecnt = np.zeros((len(lat_grid), len(lon_grid), nf))
 
     for v in range(nf):        
         for k in range(tile_coord['N_tile']):
-            if np.any([~np.isnan(tile_data[k,:])], axis=1):
+            if ~np.isnan(tile_data[k,:]):
                 j = np.searchsorted(lat_grid + resolution/2., tile_coord['com_lat'][k])
                 i = np.searchsorted(lon_grid + resolution/2., tile_coord['com_lon'][k])
-                # sometime the missing points are filled with zeros. taking largest value within
-                # the target grid cell avoids picking up unwanted zeros.
-                grid_data[j,i,v] = np.nanmax([tile_data[k,v], grid_data[j,i,v]])
-                                            
-    if grid_data.shape[-1] == 1:
-        grid_data = grid_data[:,:,0]
-        
-    return grid_data, lat_2d, lon_2d
+                # grid value takes the mean of all tiles within
+                # new_mean = old_mean + (new_value - old_mean)/count
+                N_tilecnt[j,i,v] += 1
+                grid_data[j,i,v] = grid_data[j,i,v] + (tile_data[k,v]-grid_data[j,i,v])/N_tilecnt[j,i,v]
+
+    grid_data[N_tilecnt == 0] = np.nan
+    
+    return grid_data.squeeze(), lat_2d, lon_2d
 
 # =========================== EOF ============================================================
